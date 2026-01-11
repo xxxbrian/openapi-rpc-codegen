fc43658eacddb90f8547232280dd51b0f39c9d83
Bojin Li <git@bojin.li>
2026-01-11 19:55:27 +0800

    feat: basic openapi to IR normalization

--- internal/normalize/normalize.go
+++ internal/normalize/normalize.go
@@ -1,7 +1,7 @@
 package normalize
 
 import (
-	"errors"
+	"fmt"
 
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/xxxbrian/openapi-rpc-codegen/internal/ir"
@@ -12,6 +12,24 @@ type Options struct {
 }
 
 func ToIR(doc *openapi3.T, opt Options) (*ir.Spec, error) {
-	// TODO
-	return nil, errors.ErrUnsupported
+	if doc == nil {
+		return nil, fmt.Errorf("nil OpenAPI doc")
+	}
+	baseURL := doc.Servers[0].URL
+	if opt.BaseURLOverride != "" {
+		baseURL = opt.BaseURLOverride
+	}
+
+	// TODO: implement rules here:
+	// - Only GET/POST
+	// - Must have operationId unique
+	// - Must have responses["200"].content["application/json"].schema
+	// - No oneOf/anyOf/allOf/additionalProperties, etc.
+
+	return &ir.Spec{
+		Meta:  ir.Meta{BaseURL: baseURL},
+		Types: map[string]ir.TypeDecl{},
+		// TODO: Routes
+		Routes: []ir.Route{},
+	}, nil
 }
--- internal/openapi/load.go
+++ internal/openapi/load.go
@@ -19,5 +19,9 @@ func LoadAndValidate(specPath string) (*openapi3.T, error) {
 		return nil, fmt.Errorf("OpenAPI validation error: %w", err)
 	}
 
+	if len(doc.Servers) == 0 || doc.Servers[0] == nil || doc.Servers[0].URL == "" {
+		return nil, fmt.Errorf("OpenAPI spec must define servers[0].url")
+	}
+
 	return doc, nil
 }
